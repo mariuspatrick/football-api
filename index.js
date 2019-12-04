c3e8a4b4e65ee4b62de687123b75801a40a7d647
mariuspatrick <marius.patrick72@gmail.com>
2019-12-04 14:03:06 +0100

    Added sequelize

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
+const db = require("./db");
 
 const app = express();
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.listen(port, console.log(`Listening on port: ${port}`));
 //app.get("/", (req, res) => res.send(`Listening on port: ${port}`))
