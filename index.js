dfa91b3e3c808f1f38b83da4d937cf0b3cdc18c6
mariuspatrick <marius.patrick72@gmail.com>
2019-12-04 13:39:20 +0100

    Small change

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = express();
 
 const port = process.env.PORT || 4000
 
-app.listen(port, console.log(`This is the port ${port}`));
-//app.get("/", (req, res) => res.send("hello"))
+app.listen(port, console.log(`Listening on port: ${port}`));
+//app.get("/", (req, res) => res.send(`Listening on port: ${port}`))
